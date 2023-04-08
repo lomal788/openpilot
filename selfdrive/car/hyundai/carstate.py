29f3af0c3371ff5c784c315981a36e81668da015
lomal788 <lomal788@gmail.com>
2023-04-08 14:10:34 +0900

    qq

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -83,7 +83,7 @@ class CarState(CarStateBase):
     
 
     if self.CP.carFingerprint == CAR.KIA_FORTE_KOUP_2013:
-      ret.seatbeltUnlatched = cp.vl["CLU2"]['CF_Clu_DrvSeatBeltSw'] == 0
+      ret.seatbeltUnlatched = cp.vl["CLU2"]['CF_Clu_DrvSeatBeltSw'] != 0
     else:
       ret.seatbeltUnlatched = cp.vl["CGW1"]["CF_Gway_DrvSeatBeltSw"] == 0
 
